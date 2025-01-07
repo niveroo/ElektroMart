a64f1cc16a1f91f68e3673107d55db11f9e3f6d7
Oleksandr Revin <revinsasha1970@gmail.com>
2025-01-07 19:43:33 +0100

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import HomePage from './pages/HomePage'
-import Home from './pages/HomePage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<HomePage />} />
+        </Routes>
+      </Router>
     </>
 
   )
