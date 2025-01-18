51a6c178f5bc4c1de4b4ae8fde4435bfa5e1da9c
Oleksandr Revin <revinsasha1970@gmail.com>
2025-01-18 16:20:41 +0100

    add about us section

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,27 @@
+import '../styles/AboutUs.css'
+
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <h2>About Us</h2>
+            <p>
+                At <strong>ElektroMart</strong>, we specialize in offering a wide range of cutting-edge electronics
+                from trusted global brands. From smartphones and laptops to home entertainment systems and accessories,
+                we are dedicated to providing high-quality products that meet your tech needs.
+            </p>
+
+            <div>
+                <h3>Contact Information:</h3>
+                <ul>
+                    <li><strong>Phone:</strong> +1 (800) 123-4567</li>
+                    <li><strong>Address:</strong> 123 Tech Avenue, Suite 456, Silicon City, CA 94043</li>
+                    <li>
+                        <strong>Website:</strong> <a href="https://www.ElektroMart.com" >www.ElektroMart.com</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux";
 import CategoriesList from "./CategoriesList";
+import AboutUs from "./AboutUs";
+import '../styles/CategoriesSection.css'
 
 const CategoriesSection = () => {
     const categoriesSlice = useSelector((state) => state.categories);
 
     return (
         <div className="categories-section">
+            <AboutUs />
             <CategoriesList categories={categoriesSlice.categories} />
         </div>
     );
